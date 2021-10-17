d2a2e63bed4cd7b35ecaf133ac8c59b83f0ff126
paintmeyellow <paintmeyellow.one@gmail.com>
2021-10-17 13:34:57 +0500

    add pow

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"fmt"
+	"math"
+	"math/big"
 	"strconv"
 	"time"
 )
@@ -13,6 +15,7 @@ type Block struct {
 	Data          []byte
 	PrevBlockHash []byte
 	Hash          []byte
+	Nonce         int
 }
 
 type Blockchain struct {
@@ -23,47 +26,99 @@ func NewBlockchain() *Blockchain {
 	return &Blockchain{[]*Block{NewGenesisBlock()}}
 }
 
-func (bc *Blockchain) AddBlock(b *Block) {
-	if b == nil {
-		return
-	}
+func (bc *Blockchain) AddBlock(data string) {
 	prevBlock := bc.blocks[len(bc.blocks)-1]
-	block := *b
-	block.PrevBlockHash = prevBlock.Hash
-	bc.blocks = append(bc.blocks, &block)
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.blocks = append(bc.blocks, newBlock)
 }
 
-func NewBlock(data []byte) *Block {
-	b := Block{
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{
 		Timestamp:     time.Now().Unix(),
-		Data:          data,
-		PrevBlockHash: nil,
-		Hash:          nil,
+		Data:          []byte(data),
+		PrevBlockHash: prevBlockHash,
+		Hash:          []byte{},
+		Nonce:         0,
 	}
-	b.SetHash()
-	return &b
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+	block.Hash = hash[:]
+	block.Nonce = nonce
+	return block
 }
 
 func NewGenesisBlock() *Block {
-	return NewBlock([]byte("Genesis Block"))
+	return NewBlock("Genesis Block", nil)
 }
 
-func (b *Block) SetHash() {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-	hash := sha256.Sum256(headers)
-	b.Hash = hash[:]
+const targetBits = 17
+
+type ProofOfWork struct {
+	block  *Block
+	target *big.Int
+}
+
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-targetBits))
+	return &ProofOfWork{
+		block:  b,
+		target: target,
+	}
+}
+
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+	return bytes.Join([][]byte{
+		pow.block.PrevBlockHash,
+		pow.block.Data,
+		[]byte(strconv.Itoa(int(pow.block.Timestamp))),
+		[]byte(strconv.Itoa(targetBits)),
+		[]byte(strconv.Itoa(nonce)),
+	}, []byte{})
+}
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var hashInt big.Int
+	var hash [32]byte
+	nonce := 0
+	fmt.Printf("Mining the block containing \"%s\"\n", pow.block.Data)
+	for nonce < math.MaxInt64 {
+		data := pow.prepareData(nonce)
+		hash = sha256.Sum256(data)
+		fmt.Printf("\r%x", hash)
+		hashInt.SetBytes(hash[:])
+
+		if hashInt.Cmp(pow.target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+	}
+	fmt.Print("\n\n")
+	return nonce, hash[:]
+}
+
+func (pow *ProofOfWork) Validate() bool {
+	var hashInt big.Int
+	data := pow.prepareData(pow.block.Nonce)
+	hash := sha256.Sum256(data)
+	hashInt.SetBytes(hash[:])
+	return hashInt.Cmp(pow.target) == -1
 }
 
 func main() {
+	fmt.Println("<----MINING BLOCKS---->")
 	bc := NewBlockchain()
-	bc.AddBlock(NewBlock([]byte("Send 1 BTC to Ivan")))
-	bc.AddBlock(NewBlock([]byte("Send 2 BTC to Ivan")))
+	bc.AddBlock("Send 1 BTC to Ivan")
+	bc.AddBlock("Send 2 BTC to Ivan")
 
+	fmt.Println("<----VALIDATE BLOCKS---->")
 	for _, block := range bc.blocks {
+		pow := NewProofOfWork(block)
 		fmt.Printf("Prev: %x\n", block.PrevBlockHash)
 		fmt.Printf("Hash: %x\n", block.Hash)
 		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
 		fmt.Println()
 	}
 }
